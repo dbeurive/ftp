b71129bad356335f8fbf38c5ff7be5418fc30f9c
Denis BEURIVE <dbeurive.github@gmail.com>
2019-01-18 10:56:36 +0100

    Add documentation

--- src/Exception.php
+++ src/Exception.php
@@ -1,6 +1,18 @@
 <?php
 
+/**
+ * This file implements the exception thrown by all methods in the current namespace.
+ */
+
 namespace dbeurive\Ftp;
+
+/**
+ * Class Exception
+ *
+ * This class represents the exception thrown by all methods in the current namespace.
+ *
+ * @package dbeurive\Ftp
+ */
 
 class Exception extends \Exception
 {
